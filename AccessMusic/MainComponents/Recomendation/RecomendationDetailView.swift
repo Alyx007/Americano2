7f9b0e0b3260665708dd8126395b35479c458fcc
Aleksandra <aleksandra.stupiec@gmail.com>
2025-11-18 00:21:24 +0100

    new data, recomendation view model done and some entries, minor layout changes

---
+++ AccessMusic/MainComponents/Recomendation/RecomendationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecomendationDetailView.swift
+//  AccessMusic
+//
+//  Created by Aleksandra Stupiec on 17/11/25.
+//
+
+import SwiftUI
+
+struct RecomendationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecomendationDetailView()
+}
